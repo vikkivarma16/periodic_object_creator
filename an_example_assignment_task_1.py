6d5bff382c83c9b40852047fb521b2ccc4e08056
vikkivarma16 <vikkivarma16@gmail.com>
2024-05-23 22:34:43 +0530

    Initial commit

--- an_example_assignment_task_1.py
+++ an_example_assignment_task_1.py
@@ -3,36 +3,48 @@ import math
 from math import sin, cos, tan, asin, acos, atan
 
 
+ 
 
-import example_object_simple_ring as sr 
-import chain_validity_checker as cvc
-import so_cylindrical_wrapper as wrapper
-import so_rotator as rt
-import so_translator as tl
-import periodic_chain_forger as pcf
-import vtk_particle as vp 
-import vtk_contour as vc
+#from periodic_object_creator import example_object_sample_ring as sr
+
+import periodic_object_creator.example_object_simple_ring_mod as sr
+import periodic_object_creator.chain_validity_checker_mod as cvc
+import periodic_object_creator.so_cylindrical_wrapper_mod as wrapper
+import periodic_object_creator.so_rotator_mod as rt
+import periodic_object_creator.so_translator_mod as tl
+import periodic_object_creator.periodic_chain_forger_mod as pcf
+import periodic_object_creator.vtk_particle_mod as vp 
+import periodic_object_creator.vtk_contour_mod as vc
+import periodic_object_creator.jason_file_creator_mod as js
 
 
 
 
-
-
+# chain properties
 
 chain_configuration_mode=0              # simplest chain made with horizontal and vertical rings
 N_rings=20                              # keep it always even half for horizontal and half for vertical links
 isometric_dif_factor=0                 # for the isometric deformed rings... wrapped around cylinder
-chain_radius=26.85
+chain_radius=4.0
 
 
 # rings properties
 n_beads=20  # minimum six beads are allowed
-r_curvature=5.0
+r_curvature=1.0
+
+
+print("\n\n\n...given number of beads is...", n_beads, "\n\n\n")
+print("\n\n\n... given ring radius is...", r_curvature, "\n\n\n")
+
+
+
+
 
 
 
 if (N_rings % 2!=0):
     N_rings=N_rings+1
+
 
 
 
@@ -47,20 +59,26 @@ rings_ids=[]
 
 
 # generates identical rings by calling example object ring function
-
+id_counter=0
 for i in range(N_rings):
      basis, species, bond_length, basis_bonds, basis_coordinate=sr.example_object_simple_ring(n_beads, r_curvature)
      rings_coordinate.append(basis_coordinate)
      rings_ids.append(basis)
+     
+     for j in range(len(basis_bonds)):
+        for k in range(len(basis_bonds[j])):
+            basis_bonds[j][k]=basis_bonds[j][k]+id_counter
      rings_bonds.append(basis_bonds)    
      rings_size.append(2.0*r_curvature+bond_length[0][2])
+     id_counter=id_counter+n_beads
      
      # each rings have two contact points here the contact properties are set as default which also decides the size of the rings 
      contact_bond_length.append([bond_length[0][2], bond_length[0][2]])
      
      # Check if the number of beads and the radius of the ring is compatible... otherwise high bond lengths between the beads will not let the rings fit into each other to link..
      if (2.0*r_curvature<3.5*bond_length[0][2]):
-        print("\n\n...beads are placed to too far making the torridal ring's inner radius smaller to fit the two rings in each link...\n\n")
+        print("\n\n\n...bond length is given as", bond_length[0][2], "it represents a thick toroid which would not fit in each other to form the chain...\n\n")
+        print("\n\n...beads are placed to too far increasing the bond length and making the torridal ring's inner radius smaller to fit the two rings in each link...\n\n")
         print("\n\n...please add the more number of beads...\n\n")
         print("\n\n...the process is aborted...\n\n")
         exit(0)
@@ -94,6 +112,7 @@ if (r_minimum> r_maximum):
     print("Warning:   ...incompatible configurations are given...Result: Broken or overlapping rings would be generated...")
     
 if (chain_radius <r_minimum or chain_radius> r_maximum):
+    print("Warning:   ...incompatible configurations are given...Result: Broken or overlapping rings would be generated...")
     chain_radius=0.5*(r_minimum+r_maximum)
     
     
@@ -167,6 +186,30 @@ for i in range(len(rings_size)):
 # here the final chain is being generated...    
 
 chain_coordinate=pcf.periodic_ring_chain_forge(chain_radius, rings_coordinate, rings_size, chain_configuration_mode, specific_factor)
+
+
+input_data_js=[]
+
+
+id_counter=0
+for i in range(len(chain_coordinate)):
+    molecule=[]
+    for j in range(len(chain_coordinate[i])):
+        dum=[]
+        dum.append(id_counter)
+        dum.append(rings_ids[i][j])
+        dum.append(chain_coordinate[i][j])
+        dum.append(rings_bonds[i][j] )
+
+        id_counter=id_counter+1
+        molecule.append(dum)
+    input_data_js.append(molecule)
+
+
+filename="System.json"
+
+value=js.json_writer(input_data_js, filename)
+
 
 
 
--- periodic_object_creator/jason_file_creator_mod.py
+++ periodic_object_creator/jason_file_creator_mod.py
@@ -1,40 +1,73 @@
 # periodic_object_creator/jason_file_creator
 
-def particle_vis(input_data_ps, filename):
+def json_writer(input_data_js, filename):
     
     basis=[]
     
-    for i in range(len(input_data_ps)):
-        if (input_data_ps[i][3] in basis):
-            flag=1
-        else:
-            basis.append(input_data_ps[i][3])
+    
+    import json
+
+    li = []
+    particle = []
+
+    for i in range(len(input_data_js)):
+        for j in range(len(input_data_js[i])):
+            ind_particle = { 
+                "id": input_data_js[i][j][0],
+                "species": input_data_js[i][j][1],
+                "coordinate": str( [input_data_js[i][j][2][0], input_data_js[i][j][2][1], input_data_js[i][j][2][2]]),
+                "bonded_neighbours": str(input_data_js[i][j][3])
+            }
+            particle.append(ind_particle)
+
     
     
-    for i in range(len(basis)):
-        count=0
-        for j in range(len(input_data_ps)):
-            if (input_data_ps[j][3]==basis[i]):
-                count=count+1
-        filename=filename+"particle_type_"+basis[i]+".vtk"
-        fu=open(filename, "w")
-        fu.write("# vtk DataFile Version 3.0\n")
-        fu.write("Random data to test tensors\n")
-        fu.write("ASCII\n")
-        fu.write("DATASET POLYDATA\n")
-        fu.write("POINTS  ")
-        fu.write(str(count))
-        fu.write("  float\n")
-        for j in range(len(input_data_ps)):
-	        if (input_data_ps[j][3]==basis[i]):
-	            fu.write(str(input_data_ps[j][0]))
-	            fu.write("  ")
-	            fu.write(str(input_data_ps[j][1]))
-	            fu.write("  ")
-	            fu.write(str(input_data_ps[j][2]))
-	            fu.write("\n")
-	            
-			
-			
-			
-        
+    
+    # calculate the molecular properties bonds_angle
+    total_triangle=[]
+    total_particle=[]
+    total_bonds=[]
+    for i in range(len(input_data_js)):
+        permutation=[]
+        particle_set=[]
+        bond_set=[]
+        for j in range(len(input_data_js[i])):
+            particle_set.append(input_data_js[i][j][0])
+            
+            for k in range(len(input_data_js[i][j][3])):
+                if (input_data_js[i][j][0]<input_data_js[i][j][3][k]):
+                    bond_set.append([input_data_js[i][j][0], input_data_js[i][j][3][k]])
+                for l in range( k+1, len(input_data_js[i][j][3]) ):
+                    permutation.append([input_data_js[i][j][0], input_data_js[i][j][3][k], input_data_js[i][j][3][l]])
+                    
+        total_triangle.append(permutation)
+        total_particle.append(particle_set)
+        total_bonds.append(bond_set)
+    
+    molecule=[]
+    for i in range(len(input_data_js)):
+        ind_molecule = { 
+            "beads": str(total_particle[i]),
+            "bonds": str(total_bonds[i]),
+            "bond_angle": str(total_triangle[i])
+        }
+        molecule.append(ind_molecule)
+
+    
+    
+    systems = {
+        "system": {
+            "particle": particle,
+            "molecule": molecule
+        }
+    }
+
+    # Use the filename variable correctly and open the file
+    with open(filename, "w") as fu:
+        json.dump(systems, fu, indent=4)
+
+    print("Data has been written to", filename)
+
+    
+    
+    return 0
