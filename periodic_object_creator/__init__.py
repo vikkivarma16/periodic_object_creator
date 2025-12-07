f2690b5995aab1989351bab58641ff5c9dfd1f17
Your Name <vikkivarma16@gmail.com>
2025-12-07 17:16:09 +0200

    dec_7_2025_update_update

--- periodic_object_creator/__init__.py
+++ periodic_object_creator/__init__.py
@@ -1,7 +1,9 @@
 # periodic_object_creator/__init__.py
-from .filter_broken_mol_mod import *
+
 from .assign_mol_id_mod import *
 from .export_coordinate_particle_mod import *
+from .export_bond_topology_mod import *
+from .filter_broken_mol_mod import *
 from .so_cm_calculator_mod import *
 from .so_elements_picker_mod import *
 from .so_inverter_mod import *
