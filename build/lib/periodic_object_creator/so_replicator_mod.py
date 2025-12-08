6b34998961a3d10bc0f60b887e4f1eb8b7bf0d1d
Your Name <vikkivarma16@gmail.com>
2025-12-08 21:02:14 +0200

    dec_8_2025_update_update

--- build/lib/periodic_object_creator/so_replicator_mod.py
+++
@@ -1,7 +0,0 @@
-# periodic_object_creator/so_replicator
-
-# periodic_object_creator/so_replicator_mod.py
-
-def replicator(input_object):
-    return [p[:] for p in input_object]  # deep copy preserving all attributes
-
