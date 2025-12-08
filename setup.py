bbb86b5b2ec5d2c308eea5799ad7ae1937a365c3
Your Name <vikkivarma16@gmail.com>
2025-12-08 21:10:38 +0200

    dec_8_2025_update_update_update

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name='periodic_object_creator',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "periodic_object_creator": ["*.so"],   # include all .so files
+    },
     install_requires=[
         'sympy',
     ],
-    description='A package to creat 3D, 2D and 1D objects with periodic properties.',
+    description='A package to create 3D, 2D and 1D objects with periodic properties.',
     author='vikkivarma16',
     author_email='vikkivarma16@gmail.com',
     url='https://github.com/vikkivarma16/periodic_object_creator',
